0a6b0a7e485539e7c2ba3df3af0186025d1e96e0
Scott Sampson <sampsonsclass+github@gmail.com>
2026-01-05 16:14:37 -0700

    fixed bcryptjs in students.js

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { pool } from '../db.js';
 import { verifyToken, requireRole } from '../middleware/auth.js';
 import { requirePermission } from '../middleware/permissions.js';
